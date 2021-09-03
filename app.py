b6220427510fb4bb1049936fadbe279dd743815d
PcsaDev <66335345+picsacoder@users.noreply.github.com>
2021-09-03 11:01:42 -0500

    write text

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import speech_recognition
 
-
 r = speech_recognition.Recognizer()
 
-with speech_recognition.AudioFile('englishhh.wav') as src:
+with speech_recognition.AudioFile('audio.wav') as src:
     
     owo = r.listen(src)
 
     try:
         text = r.recognize_google(owo)
         print(text)
+
+        f = open("text.txt", "w")
+        f.write(text)
+        f.close()
+
+        print("Check text.txt nwn")
     except:
         print('error')
